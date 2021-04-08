93bdfabde2f8deb51ad30122d3de7f68b268be0e
Thibaut Gautier <shanyujuneja@gmail.com>
2021-04-08 20:54:44 +0200

    Added verbose listeners

--- src/main/java/ac/artemis/anticheat/api/ArtemisAPI.java
+++ src/main/java/ac/artemis/anticheat/api/ArtemisAPI.java
@@ -1,9 +1,16 @@
 package ac.artemis.anticheat.api;
 
+import ac.artemis.anticheat.api.listener.VerboseListener;
 import cc.ghast.packet.nms.ProtocolVersion;
 
 import java.util.UUID;
 
 public interface ArtemisAPI {
     ProtocolVersion getVersion(UUID uuid);
+
+    void addVerboseListener(VerboseListener verboseListener);
+
+    void removeVerboseListener(VerboseListener verboseListener);
+
+    void clearVerboseListeners();
 }
---
+++ src/main/java/ac/artemis/anticheat/api/alert/Severity.java
@@ -0,0 +1,6 @@
+package ac.artemis.anticheat.api.alert;
+
+public enum Severity {
+    VERBOSE,
+    VIOLATION;
+}
